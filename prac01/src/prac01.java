c83b74d5824bd2729e6e3af2d36c905e1efae70a
ParkSungGyu1 <steven0507@naver.com>
2022-05-13 11:10:56 +0900

    풀이법 추가

--- prac01/src/prac01.java
+++ prac01/src/prac01.java
@@ -1,3 +1,14 @@
+
+/*
+    프로그래머스 가운데 글자 가져오기
+    https://programmers.co.kr/learn/courses/30/lessons/12903
+
+    풀이과정
+    1. 문자열이 짝수일 경우와 홀수일 경우를 조건문으로 분리한다.
+    2. 홀수일 경우 문자열 길이의 절반에 해당하는 문자열을 반환한다.
+    3. 짝수일 경우 문자열 길이의 절반에서 -1~+1 까지 범위의 문자열을 반환한다.
+   
+ */
 class Solution {
     String answer = "";
     public String solution(String s) {
