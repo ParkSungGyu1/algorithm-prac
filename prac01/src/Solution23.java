65faf8de746b8860bf40d246335b0011d018ef7e
ParkSungGyu1 <steven0507@naver.com>
2022-05-16 14:03:35 +0900

    알고리즘 공부 22.05.16

---
+++ prac01/src/Solution23.java
@@ -0,0 +1,22 @@
+
+/*
+    프로그래머스 완주하지 못한 선수
+    https://programmers.co.kr/learn/courses/30/lessons/42576
+
+    풀이과정
+    1.
+
+ */
+class My_Solution23 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+}
+
+public class Solution23 {
+    public static void main(String[] args) {
+        Solution2 a = new Solution2();
+
+    }
+}
