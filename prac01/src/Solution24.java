9449536a2379ca731c50ebec88501b7291f9dbab
ParkSungGyu1 <steven0507@naver.com>
2022-05-16 15:37:39 +0900

    알고리즘 공부 22.05.16

--- prac01/src/Solution24.java
+++ prac01/src/Solution24.java
@@ -1,21 +1,33 @@
 
 /*
-    프로그래머스 자릿수 더하기
-    https://programmers.co.kr/learn/courses/30/lessons/12931
+    프로그래머스 자연수 뒤집어 배열로 만들기
+    https://programmers.co.kr/learn/courses/30/lessons/12932
     풀이과정
-    1. Stream을 활용하여 n을 list화 해준다.
-    2. stream.sum을 이용해 합을 구한다.
+    1. n의 자릿수만큼 k를 증가시킨다.
+    2. k의 길이만큼의 배열을 선언한다.
+    3. 입력받은 정수를 10의 단위로 잘라가면서 저장한다.
 
  */
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.*;
+class Solution24 {
+    public int[] solution(long n) {
+        int[] answer = {};
+        int k = 0;
+        long num = n;
 
-public class Solution24 {
-    public int solution(int n) {
+        while(n>0){
+            k++;
+            n = n/10;
+        }
 
-        int[] cal = Stream.of(String.valueOf(n).split("")).mapToInt(Integer::parseInt).toArray();
+        answer = new int[k];
 
-        return Arrays.stream(cal).sum();
+        for(int i=0; i<k; i++){
+            answer[i] = (int)(num%10);
+            num = num/10;
+        }
+
+        return answer;
     }
 }
