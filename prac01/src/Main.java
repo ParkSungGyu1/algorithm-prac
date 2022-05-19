c2f609502bbba4b55a4731bf42372476992387a7
ParkSungGyu1 <steven0507@naver.com>
2022-05-19 15:49:37 +0900

    알고리즘 공부 22.05.19

--- prac01/src/Main.java
+++ prac01/src/Main.java
@@ -1,24 +1,47 @@
+import java.util.*;
+
 public class Main {
-    public String solution(int month, int day) {
-        int[] my_day = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-        int left_day = 98 - (my_day[month]-day);
-        month += 1;
-        while(true){
-            if(left_day - my_day[month] < 0){
-                break;
-            }
-            left_day -=  my_day[month];
-            month += 1;
-            if(month > 12){
-                month = 1;
+    public String[] solution(String[] arr, int n) {
+        String[] answer = {};
+        String garb = "";
+        Arrays.sort(arr);
+        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> garbList = new ArrayList<>();
+
+        for(String item : arr){
+            if(!arrayList.contains(item))
+                arrayList.add(item);
+            else{
+                garbList.add(item);
             }
         }
-        String answer = Integer.toString(month)+"월 "+Integer.toString(left_day)+"일";
+
+        arrayList.removeAll(garbList);
+
+        for(int i = 1; i < arrayList.size(); i++){
+            for(int j = 0; j < arrayList.size() - i; j++){
+                if(arrayList.get(j).charAt(n) > arrayList.get(j+1).charAt(n)){
+                    garb = arrayList.get(j);
+                    arrayList.set(j,arrayList.get(j+1));
+                    arrayList.set(j+1,garb);
+                }
+            }
+
+
+        }
+        answer = new String[arrayList.size()];
+        int size=0;
+
+        for(String temp : arrayList){
+            answer[size++] = temp;
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         Main method = new Main();
-        System.out.println(method.solution(1, 18));
+        String[] arr = {"coke", "water", "glass", "dog", "dog", "yogurt", "vitamin"};
+        int n = 2;
+        System.out.println(Arrays.toString(method.solution(arr, n)));
     }
 }
